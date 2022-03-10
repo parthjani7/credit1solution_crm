e16141c919e0f35d8630a0f4de970022f0170c45
Parth Jani <parthjani4@gmail.com>
2022-03-10 14:46:41 -0500

    middleware removed as of non-use

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\CrmAuth;
 use App\Http\Middleware\TokenVerifier;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -65,7 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'token' => TokenVerifier::class,
-        'crmauth'=> CrmAuth::class
+        'token' => TokenVerifier::class
     ];
 }
