5bdbb4cc9329d693fd6b659b4476ee0586ad6393
Parth Jani <parthjani4@gmail.com>
2022-03-11 22:43:55 -0500

    clumn updated to nullable

--- database/migrations/2015_08_08_121130_create_email_details.php
+++ database/migrations/2015_08_08_121130_create_email_details.php
@@ -4,26 +4,25 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
 
 
-	public function up()
-	{
-		Schema::create('email_details', function(Blueprint $table)
-		{
-			$table->id();
-			$table->string("to_from");
-			$table->string("type",50);
-			$table->string("subject");
-			$table->text("message");
-			$table->tinyInteger("include_data");
-			$table->timestamps();
-		});
-	}
+    public function up()
+    {
+        Schema::create('email_details', function (Blueprint $table) {
+            $table->id();
+            $table->string("to_from", 150)->nullable();
+            $table->string("type", 20);
+            $table->string("subject");
+            $table->text("message");
+            $table->tinyInteger("include_data");
+            $table->timestamps();
+        });
+    }
 
-	public function down()
-	{
-		Schema::dropIfExists('email_details');
-	}
-
+    public function down()
+    {
+        Schema::dropIfExists('email_details');
+    }
 };
