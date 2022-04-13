ed9e9fe899b5c02db54bffcea190601fb6d62707
Parth Jani <parthjani4@gmail.com>
2022-04-13 16:36:29 -0400

    removed force ssl, using htaccess

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,10 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(!$this->app->environment('local')) {
-            URL::forceScheme('https');
-        }
-
         $this->app->bind(
 			'Illuminate\Contracts\Auth\Registrar',
 			'App\Services\Registrar',
